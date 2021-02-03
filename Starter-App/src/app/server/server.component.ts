4702ebee4bad8319f3668d0cb2d0a110bb20c9f7
Punith KR <68544398+punithkravi007@users.noreply.github.com>
2021-02-03 08:35:01 +0530

    Angular Starter Project

---
+++ Starter-App/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {
+  author_name = 'Punith KR';
+  about_author = `Carlos Ray "Chuck" Norris is an American martial artist, actor, film producer, and screenwriter. 
+                  After serving in the United States Air Force, Norris won many martial arts championships and later 
+                  founded his own discipline Chun Kuk Do`;
+}
