83f43b64646fb9eca0c7a6fce6db4bbc76607464
Punith KR <68544398+punithkravi007@users.noreply.github.com>
2021-02-07 17:47:40 +0530

    Assignment01 Updated

--- Starter-App/src/app/assignment01/assignment01.component.ts
+++ Starter-App/src/app/assignment01/assignment01.component.ts
@@ -3,32 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-assignment01',
   templateUrl: './assignment01.component.html',
-  styleUrls: ['./assignment01.component.css']
+  styleUrls: ['./assignment01.component.css'],
 })
 export class Assignment01Component implements OnInit {
-
-  assignmentName : string = "Server Managment";
-  isRunning : boolean = false;
+  assignmentName: string = 'Server Managment';
+  servers: any = [];
   serverId: number | string;
-  serverStatus: string;
-  isServerAdded : boolean = false;
+  serverPort : number | string;
+  serverDescription : string;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  getServerStatus() {
-    return this.isRunning ? ["Running...", "darkseagreen"] : ["Stopped!", "red"];
+  Server = function (id: number | string, isRunning: boolean, port : number|string, description : string) {
+    this.id = id;
+    this.isRunning = isRunning;
+    this.port = port;
+    this.description = description;
+
+    this.getServerStatus = () => {
+      return this.isRunning ? 'Running...' : 'Stopped';
+    };
+
+    this.toggleServerStatus = () => {
+      this.isRunning = !this.isRunning;
+    };
+  };
+
+  addServer() {
+    let server: any = new this.Server(this.serverId, false, this.serverPort, this.serverDescription);
+    this.servers.push(server);
+    this.serverId = '';
   }
-
-  toggleServerStatus(){
-    this.isRunning = !this.isRunning;
-  }
-
-  addOrRemoveServer(){
-    this.serverId = this.isServerAdded ? "" : this.serverId;
-    this.isServerAdded = !this.isServerAdded;
-  }
-
-
 }
